d282e16eaeaba0b32b6f7b86d6f7db91ea06323d
pforbes <pforbes@wisc.edu>
2017-12-12 13:27:12 -0600

    CUDA works v1

--- myODE1_CUDA.cu
+++ myODE1_CUDA.cu
@@ -1,229 +1,215 @@
-/*
- Copyright 2010-2012 Karsten Ahnert
- Copyright 2011-2013 Mario Mulansky
- Copyright 2013 Pascal Germroth
- Distributed under the Boost Software License, Version 1.0.
- (See accompanying file LICENSE_1_0.txt or
- copy at http://www.boost.org/LICENSE_1_0.txt)
- */
-
-
 #include <iostream>
 #include <vector>
-#include <complex>
-#include <boost/numeric/odeint.hpp>
+#include "cuda_complex.hpp"
 #include <math.h>
 #include <random>
 #include <chrono>
 #include "stopwatch.hpp"
-#include <omp.h>
 #include <cuda.h>
-
-using namespace std;
-using namespace boost::numeric::odeint;
-
-
-//[ rhs_function
-/* The type of container used to hold the state vector */
-typedef vector< complex< double > > state_type;
-
 	
 const double b_sigma = 0.01; // normal distribution std deviation for b
 /* set b0 values to increment: # points = (MAX-MIN)/INCR+1 */
 const double MIN_b0 = 1.515;
 const double MAX_b0 = 1.52;
 const double INCR_b0 =0.00001;
+const int SIZE_b0 = int((MAX_b0-MIN_b0)/INCR_b0)+1;
 
 /* set C values to increment: # points = (MAX-MIN)/INCR+1 */
 const double MIN_C = 0.019;
 const double MAX_C = 0.020;
 const double INCR_C= 0.0001;
-
+const int SIZE_C = int((MAX_C-MIN_C)/INCR_C)+1;
+  
 /* Set resolution in length along device ('time' for ODE solvers) */
 const double L_max = 150.0;
-const double L_inc = 0.001;
+//const double L_inc = 0.001;
 
 const int B_LENGTH = int(L_max) + 1;  // length of the b vector
-const double d = 0;
-const double QC = 1.85*0.25;
+//const double d = 0;
+//const double QC = 1.85*0.25;
 
-const complex<double> i(0,1);
-
-
-//[ rhs_class
-/* The rhs of x' = f(x) defined as a class */
-class ODE_SOLVER{
-
-  double& m_b;
-  const int mb_size;
-  const double mC;
-  const double mb0;
-
-
-public:
-  ODE_SOLVER( double& b, int b_length, double C , double b0) : m_b(b), mb_size(b_length),  mC(C), mb0(b0) { }
-
-  void operator()( const state_type &x , state_type &dxdt , double  t  )
-  {
-    int T_low = int(floor(t));
-    int T_high = T_low + 1;
-    if (T_low >= mb_size-1) {
-       T_high = T_low;
-    }
-    double b_interp =  mb0 + m_b[T_low] + double(t-T_low)*(m_b[T_high]-m_b[T_low]);
-    dxdt[0] = x[1];
-    dxdt[1] = x[2];
-    dxdt[2] = -i*mC*mC*mC*((4.0*QC)*(b_interp+i*d)-1.0)*x[0] - mC*mC*4.0*QC*x[1] -i*mC*(b_interp+i*d)*x[2];
-  }
-};
-//]
-
-struct max_observer
-{
-  double mC;
-  double &mGain;
-  double &mTime;
-  
-  max_observer(double C, double &maxGain, double &maxTime)
-    :  mC( C ), mGain(maxGain), mTime( maxTime ) { }
-
-  void operator()( const state_type &x , double t )
-    {
-      double GAIN = std::pow(abs( 1.0/(x[2] + 4.0*QC*mC*mC*x[0])),2);
-      if ( mGain < GAIN)
-	{
-	  mTime = t;
-	  mGain = GAIN;
-	}
-    }
-};
 
 __global__ void ODE_Kernel(double* db0_in, double* dC_in,double *db_rand, double* dLength,double* dGain, int num_loops, int b_length)
 {
-
-  int idx = blockIdx.x + threadIdx.x;
+  double dt = 0.001;
+  int idx = blockIdx.x*blockDim.x + threadIdx.x;
+  
+  //
+  // should implement shared memory method for interpolating b(x)
+  //
+  
   if (idx < num_loops)
     {
       double b0 = db0_in[idx];
       double C  = dC_in[idx];
+      double QC = 0.25*1.85;
+      double d = 0.0;
       
-	//state initialization
-      state_type x(3);
-      x[0] = 0.0;
-      x[1] = 0.0;
-      x[2] = 1.0;
+      //state initialization
+      complex<double> x0(0.0,0.0);
+      complex<double> x1(0.0,0.0);
+      complex<double> x2(1.0,0.0);
+
+      complex<double> i(0,1);
       
-      // //[ define_const_stepper
-      runge_kutta4< state_type> stepper;
-      
-	// initiate ODE solver class
-      ODE_SOLVER myODE(db_rand, b_length, C, b0);
-      
-      // Solve ODE using class
-      size_t steps = integrate_const(stepper,  myODE ,
-				     x , 0.0 , L_max , L_inc,
-				     max_observer(C, dGain[idx], dLength[idx]) );
-      
-      dGain[idx] = 10*std::log10(dGain[idx]);
+      // Solve ODE using
+	for(int n = 0; n < 150000; n++) {
+
+	//[ this should be replace with shared memory above
+	// calculate current b 
+	int T_low = int(floor(n*dt));
+	int T_high = T_low + 1;
+	if (T_low >= b_length-1) {
+	  T_high = T_low;
+	}
+	double b =  b0 + db_rand[T_low] + double(n*dt-T_low)*(db_rand[T_high]-db_rand[T_low]);
+	
+	//]
+
+	
+	//[ it seems k and l can be simplified to reduce # of registers used i.e. k1 = 1.5*k0, k2 = 2.25*k0
+	complex<double> k0 = dt*x1;
+	complex<double> l0 = dt*x2;
+	complex<double> m0 = dt*(-i*C*C*C*(4.0*QC*(b+i*d)-1.0)*x0 - 4.0*QC*C*C*x1 - i*C*(b + i*d)*x2);
+	complex<double> k1 = dt*(x1+0.5*l0);
+	complex<double> l1 = dt*(x2+0.5*m0);
+	complex<double> m1 = dt*(-i*C*C*C*(4.0*QC*(b+i*d)-1.0)*(x0+0.5*k0) - 4.0*QC*C*C*(x1+0.5*l0) - i*C*(b + i*d)*(x2+0.5*m0));;
+	complex<double> k2 = dt*(x1+0.5*l1);
+	complex<double> l2 = dt*(x2+0.5*m1);
+	complex<double> m2 = dt*(-i*C*C*C*(4.0*QC*(b+i*d)-1.0)*(x0+0.5*k1) - 4.0*QC*C*C*(x1+0.5*l1) - i*C*(b + i*d)*(x2+0.5*m1));;
+	complex<double> k3 = dt*(x1+0.5*l2);
+	complex<double> l3 = dt*(x2+0.5*m2);
+	complex<double> m3 = dt*(-i*C*C*C*(4.0*QC*(b+i*d)-1.0)*(x0+0.5*k2) - 4.0*QC*C*C*(x1+0.5*l2) - i*C*(b + i*d)*(x2+0.5*m2));;
+	x0 = x0 + 1.0/6.0*(k0 + k1+k1 + k2+k2 + k3);
+	x1 = x1 + 1.0/6.0*(l0 + l1+l1 + l2+l2 + l3);
+	x2 = x2 + 1.0/6.0*(m0 + m1+m1 + m2+m2 + m3);
+	//]
+	
+	// calculate gain(x)
+	double GAIN = std::pow(abs( 1.0/(x2 + 4.0*QC*C*C*x0)),2);
+	
+	// test for best gain
+	if ( dGain[idx] < GAIN)
+	  {
+	    dLength[idx] = n*dt; // store current length if best gain
+	    dGain[idx] = GAIN; // store Gain if best gain
+	  }
+	}
+	// change gain to dB units
+      	dGain[idx] =  10*std::log10(dGain[idx]);
     }
 }
 
 
-// struct write_state
-// {
-//     void operator()( const state_type &x ) const
-//     {
-//         std::cout << x[0] << "\t" << x[1] << "\n";
-//     }
-// };
-
-
 int main(int  argc , char*  argv[]  )
 {
-  int num_procs;
-  if( argc == 2) {
-    num_procs = atoi(argv[1]);
-  } else {
-    num_procs = omp_get_num_procs();
-  }
-
   stopwatch<std::milli, float> sw_incl;
   sw_incl.start();
   
-  vector <double> b0_in;
-  for (double b0 = MIN_b0; b0 <= MAX_b0; b0 += INCR_b0) {
-    b0_in.push_back(b0);
+  int num_loops = SIZE_b0*SIZE_C;
+  // vectors for input data
+  double *b0_in = (double*)malloc(sizeof(double)*SIZE_b0);
+  double *C_in = (double*)malloc(sizeof(double)*SIZE_C);
+
+  // create b0_in and C_in vectors
+  b0_in[0] = MIN_b0;
+  C_in[0] = MIN_C;
+  for (int b = 1; b < SIZE_b0; b++) {
+    b0_in[b] = b0_in[b-1] + INCR_b0;
   }
-  
-  vector <double> C_in;
-  for (double C = MIN_C; C <= MAX_C; C += INCR_C) {
-    C_in.push_back(C);
+  for (int c = 1; c < SIZE_C; c++) {
+    C_in[c] = C_in[c-1] + INCR_C;
   }
-  
-  vector <double> C_out;
-  vector <double> b0_out;
-  for (double b0 = MIN_b0; b0 <= MAX_b0; b0 += INCR_b0) {
-    for (double C = MIN_C; C <= MAX_C; C += INCR_C) {
-      C_out.push_back(C);
-      b0_out.push_back(b0);
+
+  // vectors for output data
+  double *C_out = (double*)malloc(sizeof(double)*num_loops);
+  double *b0_out = (double*)malloc(sizeof(double)*num_loops);
+  double *b_rand = (double*)malloc(sizeof(double)*B_LENGTH);
+  double *gain_dB_out = (double*)malloc(sizeof(double)*num_loops);
+  double *length_out = (double*)malloc(sizeof(double)*num_loops);
+
+  // create C_out and b0_out vectors
+  int idx = 0;
+  for (int b = 0; b<SIZE_b0; b++) {
+    for (int c = 0; c<SIZE_C; c++) {
+      b0_out[idx] = b0_in[b];
+      C_out[idx] = C_in[c];
+      idx++;
     }
   }
-  
-  int num_loops = C_in.size()*b0_in.size();
-
-  // vectors to store output
-  vector <double> gain_dB_out(num_loops);
-  vector <double> length_out(num_loops);
 
   // set up random b(x) distribution
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   std::default_random_engine generator (seed);
   std::normal_distribution<double> distribution(0,b_sigma);
-  std::vector <double> b_rand;
   for (int n = 0; n < B_LENGTH; n++) {
-    b_rand.push_back(distribution(generator));
+    b_rand[n] = distribution(generator);
   }
 
+  std::cout << SIZE_b0 << ' '  << ' ' << SIZE_C << '\n';
+  std::cout << b0_in[0] << ' ' << b0_in[SIZE_b0-1] << '\n';
   
-  /* Allocate device memory */
+  // Allocate device memory 
   double *db0_in, *dC_in, *db_rand, *dLength, *dGain;
+
+  // START TIMER
+  cudaEvent_t startTime, stopTime;
+  float time;
+  cudaEventCreate(&startTime);
+  cudaEventCreate(&stopTime);
+  cudaEventRecord(startTime,0);
+
   cudaMalloc((void**)&db0_in, sizeof(double)*num_loops);
   cudaMalloc((void**)&dC_in, sizeof(double)*num_loops);
-  cudaMalloc((void**)&db_rand, sizeof(double)*b_rand.size());
+  cudaMalloc((void**)&db_rand, sizeof(double)*B_LENGTH);
   cudaMalloc((void**)&dLength, sizeof(double)*num_loops);
   cudaMalloc((void**)&dGain, sizeof(double)*num_loops);
-  
-  /* send all data to GPU */
-  cudaMemcpy(&b0_in, db0_in, sizeof(double)*num_loops, cudaMemcpyDeviceToHost);
-  cudaMemcpy(&C_in, dC_in, sizeof(double)*num_loops, cudaMemcpyDeviceToHost);
-  cudaMemcpy(&b_rand, db_rand, sizeof(double)*b_rand.size(), cudaMemcpyDeviceToHost);
+
+  // send all data to GPU 
+  cudaMemcpy(db0_in, b0_out, sizeof(double)*num_loops, cudaMemcpyHostToDevice);
+  cudaMemcpy(dC_in, C_out, sizeof(double)*num_loops, cudaMemcpyHostToDevice);
+  cudaMemcpy(db_rand, b_rand, sizeof(double)*B_LENGTH, cudaMemcpyHostToDevice);
   cudaMemset(dGain, 0, sizeof(double)*num_loops);
-  cudaMemset(dLength, -1, sizeof(double)*num_loops);
+  cudaMemset(dLength, 0, sizeof(double)*num_loops);
 
-  /* Call Cuda Kernels to solve ODE */
-  ODE_Kernel<<<16,16>>>(db0_in,dC_in,db_rand,dLength,dGain, num_loops, B_LENGTH);
+  // Call Cuda Kernels to solve ODE
+  int num_threads = 256;
+  int num_blocks = 22;
+
+  ODE_Kernel<<<num_blocks,num_threads>>>(db0_in,dC_in,db_rand,dLength,dGain, num_loops, B_LENGTH);
+
+  cudaMemcpy(length_out, dLength, sizeof(double)*num_loops, cudaMemcpyDeviceToHost);
+  cudaMemcpy(gain_dB_out, dGain, sizeof(double)*num_loops, cudaMemcpyDeviceToHost);  
   
-  /* send results back to GPU */
-  cudaMemcpy(&gain_dB_out, dGain, sizeof(double)*num_loops, cudaMemcpyDeviceToHost);
-  cudaMemcpy(&length_out, dLength, sizeof(double)*num_loops, cudaMemcpyDeviceToHost);
+  // stop inclusive timer
+  cudaEventRecord(stopTime,0);
+  cudaEventSynchronize(stopTime);
+  cudaEventElapsedTime(&time, startTime, stopTime);
+  cudaEventDestroy(startTime);
+  cudaEventDestroy(stopTime);
 
+  // print output data to terminal
+  std::cout  << 'C' << '\t' << "b0" << '\t' << "maxLength" << '\t' << "maxGain" << '\t' << "maxGain[dB]" << std::endl;  
+  std::cout << "---------------------------------------------------" << std::endl;
+  for(int n = 0; n < num_loops; n++) {
+    std::cout << C_out[n] << '\t' << b0_out[n] << '\t' << length_out[n] << '\t' << gain_dB_out[n] << '\t'<< n << '\n';
+  }   
+  std::cout << "time[ms]" << '\t' << "num_loops" << '\t'<< "ms/loop" << '\n';
+  std::cout << time << '\t' << num_loops << '\t'<< time/float(num_loops) << '\n';
+
+  // free device memory
   cudaFree(db0_in);
   cudaFree(dC_in);
   cudaFree(db_rand);
   cudaFree(dLength);
   cudaFree(dGain);
-	   
-  // stop inclusive timer
-  sw_incl.stop();
-  float time = sw_incl.count();
 
-  // print output data to terminal
-  cout  << 'C' << '\t' << "b0" << '\t' << "maxLength" << '\t' << "maxGain" << '\t' << "maxGain[dB]" << std::endl;  
-  cout << "---------------------------------------------------" << std::endl;
-  for(int n = 0; n < num_loops; n++) {
-    cout << C_out[n] << '\t' << b0_out[n] << '\t' << length_out[n] << '\t' << gain_dB_out[n] << '\n';
-  }   
-  cout << "n_procs" << '\t' << "time[ms]" << '\t' << "num_loops" << '\t'<< "ms/loop" << '\n';
-  cout << num_procs << '\t' << time << '\t' << num_loops << '\t'<< time/float(num_loops) << '\n';
-    
+  // free host memory
+  free(length_out);
+  free(gain_dB_out);
+  free(C_in);
+  free(C_out);
+  free(b0_in);
+  free(b0_out);
+  free(b_rand);
 }
